a17f5f3749609dbb27dc3bb42027f50775e85fa3
kamiark <kamiark@0938d2e6-47e9-45e5-8a4d-920110a584dc>
2015-09-04 09:08:01 +0000

    Re-factored example0.c

--- trunk/examples/example0/example0.c
+++ trunk/examples/example0/example0.c
@@ -1,6 +1,6 @@
 /*
- * example0.c
- * It shows how to use the _settext/_findnext interface of the ahocorasick API
+ * example0.c It explains how to use the _settext/_findnext function pair of
+ * the ahocorasick library
  * 
  * This file is part of multifast.
  *
@@ -24,7 +24,7 @@
 #include <string.h>
 #include "ahocorasick.h"
 
-AC_ALPHABET_t * sample_patterns[] = {
+AC_ALPHABET_t *sample_patterns[] = {
     "city",
     "clutter",
     "ever",
@@ -37,14 +37,15 @@ AC_ALPHABET_t * sample_patterns[] = {
 };
 #define PATTERN_COUNT (sizeof(sample_patterns)/sizeof(AC_ALPHABET_t *))
 
-AC_ALPHABET_t * text1 = "experience the ease and simplicity of multifast";
-AC_ALPHABET_t * text2 = "whatever you are be a good one";
-AC_ALPHABET_t * text3 = "out of clutter, find simplicity";
+AC_ALPHABET_t *chunk1 = "experience the ease and simplicity of multifast";
+AC_ALPHABET_t *chunk2 = "whatever you are be a good one";
+AC_ALPHABET_t *chunk3 = "out of clutter, find simplicity";
 
+void print_match (AC_MATCH_t *m);
 
-int main (int argc, char ** argv)
+int main (int argc, char **argv)
 {
-    unsigned int j;
+    unsigned int i;
     AC_TRIE_t *trie;
     AC_PATTERN_t patt;
     AC_TEXT_t chunk;
@@ -53,19 +54,19 @@ int main (int argc, char ** argv)
     /* Get a new trie */
     trie = ac_trie_create ();
     
-    for (j = 0; j < PATTERN_COUNT; j++)
+    for (i = 0; i < PATTERN_COUNT; i++)
     {
         /* Fill the pattern data */
-        patt.ptext.astring = sample_patterns[j];
+        patt.ptext.astring = sample_patterns[i];
         patt.ptext.length = strlen(patt.ptext.astring);
         
-        /* The replacement pattern is not applicable, so better to initialize 
-         * it with 0 */
+        /* The replacement pattern is not applicable in this program, so better 
+         * to initialize it with 0 */
         patt.rtext.astring = NULL;
         patt.rtext.length = 0;
         
         /* Pattern identifier is optional */
-        patt.id.u.number = j + 1;
+        patt.id.u.number = i + 1;
         patt.id.type = AC_PATTID_TYPE_NUMBER;
         
         /* Add pattern to automata */
@@ -87,10 +88,10 @@ int main (int argc, char ** argv)
     /* Display the trie if you wish */
     // ac_trie_display (trie);
     
-    printf ("Searching: \"%s\"\n", text1);
+    printf ("Searching: \"%s\"\n", chunk1);
     
-    chunk.astring = text1;
-    chunk.length = strlen(chunk.astring);
+    chunk.astring = chunk1;
+    chunk.length = strlen (chunk.astring);
     
     /* Set the input text */
     ac_trie_settext (trie, &chunk, 0);
@@ -102,67 +103,62 @@ int main (int argc, char ** argv)
     /* Find matches */
     while ((match = ac_trie_findnext(trie)).size)
     {
-        printf ("@%2lu: ", match.position);
-        
-        for (j = 0; j < match.size; j++)
-            printf("#%ld (%s), ", 
-                    match.patterns[j].id.u.number, 
-                    match.patterns[j].ptext.astring);
-        
-        /* CAUTION: the AC_PATTERN_t::ptext.astring pointers, point to the 
-         * sample patters in our program, since we added patterns with copy 
-         * option disabled. 
-         */
-        
+        print_match (&match);
         printf ("\n");
     }
     
-    printf ("Searching: \"%s\"\n", text2);
+    printf ("Searching: \"%s\"\n", chunk2);
     
-    chunk.astring = text2;
-    chunk.length = strlen(chunk.astring);
+    chunk.astring = chunk2;
+    chunk.length = strlen (chunk.astring);
     
     /* Set the input text for a new search (keep = 0) */
     ac_trie_settext (trie, &chunk, 0);
     
     while ((match = ac_trie_findnext(trie)).size)
     {        
-        printf ("@%2lu: ", match.position);
-        
-        for (j = 0; j < match.size; j++)
-            printf("#%ld (%s), ", 
-                    match.patterns[j].id.u.number, 
-                    match.patterns[j].ptext.astring);
-        
+        print_match (&match);
         printf ("\n");
     }
     
-    printf ("Searching: \"%s\"\n", text3);
+    printf ("Searching: \"%s\"\n", chunk3);
     
-    chunk.astring = text3;
-    chunk.length = strlen(chunk.astring);
+    chunk.astring = chunk3;
+    chunk.length = strlen (chunk.astring);
     
     /* Set the input text as the successor chunk of the previous one */
     ac_trie_settext (trie, &chunk, 1);
     
     /* When the keep option (3rd argument) in set, then the automata considers 
-     * that the given text is the next chunk of the previous text. To see the 
+     * that the given text is the next chunk of the previous one. To see the 
      * difference try it with 0 and compare the result */
     
     while ((match = ac_trie_findnext(trie)).size)
     {        
-        printf ("@%2lu: ", match.position);
-        
-        for (j = 0; j < match.size; j++)
-            printf("#%ld (%s), ", 
-                    match.patterns[j].id.u.number, 
-                    match.patterns[j].ptext.astring);
-        
+        print_match (&match);
         printf ("\n");
     }
     
     /* You may release the automata after you have done with it. */
     ac_trie_release (trie);
-        
+    
     return 0;
+}
+
+void print_match (AC_MATCH_t *m)
+{
+    unsigned int j;
+    
+    printf ("@%2lu: ", m->position);
+    
+    for (j = 0; j < m->size; j++)
+    {
+        printf("#%ld (%.*s), ", m->patterns[j].id.u.number,
+            (int)m->patterns[j].ptext.length, m->patterns[j].ptext.astring);
+    }
+    
+    /* CAUTION: the AC_PATTERN_t::ptext.astring pointers, point to the 
+     * sample patters in our program, since we added patterns with copy 
+     * option disabled.
+     */
 }
